284271ef6f5c76ce8ff46d81f3ab01bb7d16eb5e
draguar <guillaume.vanel@protonmail.com>
2021-11-30 09:44:25 +0100

    finished hands on

---
+++ src/README.md
@@ -0,0 +1,4 @@
+# Hands On: Deploy a python package
+Don't worry about the code, it's just an old simulation project that I deploye in the context of the hands-on based on Sergio Peignier's teaching, available at https://sergiopeignier.github.io/tutorial_python_packaging.html
+
+The hands-on content can be found in [the project's wiki](https://github.com/draguar/HandsOn_DeployAPythonPackage/wiki)
---
+++ src/setup.py
@@ -0,0 +1,13 @@
+#!/use/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='HandsOnDeployAPackage',
+      version='1.4',
+      description='Hands-on: deploy a Python package',
+      author='Guilom',
+      author_email='guillaume.vanel@protonmail.com',
+      url='https://github.com/draguar/HandsOn_DeployAPythonPackage',
+      py_modules=['ourCode'],
+      license_files = '../LICENSE')
