19dca7aa6da9687a716fdefcc45afcdb2923e8ec
draguar <guillaume.vanel@protonmail.com>
2021-12-02 10:49:31 +0100

    added Dockerfile and requirements in setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,14 @@
-#!/use/bin/env python
+#!/usr/bin/env python
 
 import setuptools
 from distutils.core import setup
 
 setup(name='HandsOnDeployAPackage',
-      version='1.4',
+      version='1.7',
       description='Hands-on: deploy a Python package',
       author='Guilom',
       author_email='guillaume.vanel@protonmail.com',
       url='https://github.com/draguar/HandsOn_DeployAPythonPackage',
       py_modules=['ourCode'],
-      license_files = '../LICENSE')
+      license_files = '../LICENSE',
+      install_requires = ['pandas>=1.3.4', 'matplotlib>=3.5.0'])
